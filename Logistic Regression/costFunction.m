6688eeddfc05cf74ed9d6a6f20c35a9a0400e8a8
Arondite <stephenknotek@gmail.com>
2016-10-19 10:40:28 -0600

    Add files via upload

---
+++ Logistic Regression/costFunction.m
@@ -0,0 +1,58 @@
+function [J, grad] = costFunction(theta, X, y)
+%COSTFUNCTION Compute cost and gradient for logistic regression
+%   J = COSTFUNCTION(theta, X, y) computes the cost of using theta as the
+%   parameter for logistic regression and the gradient of the cost
+%   w.r.t. to the parameters.
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost of a particular choice of theta.
+%               You should set J to the cost.
+%               Compute the partial derivatives and set grad to the partial
+%               derivatives of the cost w.r.t. each parameter in theta
+%
+% Note: grad should have the same dimensions as theta
+%
+hypothesis = sigmoid(X*theta);
+
+%J = sum( (-y) * log(hypothesis) + ( 1 - y) * log(1 - hypothesis)) / ( m )
+
+J = sum(y .* log(hypothesis) + (1 - y) .* log(1 - hypothesis));
+J = J * (-1/m)
+
+%for j = 1:length(theta)
+%  J += (theta(j) -5)^2	
+%end
+
+%for k = 1:m  
+ % for i = 1:length(theta)
+  %  temp(i) = grad(i) + (1/m) * sum( (hypothesis(i) - y(i)) .* X(:, i) );
+   % grad(i) = temp(i);
+ % end
+%end
+
+for k = 1:m  
+    grad = (grad +(hypothesis(k) - y(k)) .* X(k, :)' );
+end
+
+%for k = 1:m
+%  grad = sum(hypothesis(k) - y(k) .* X(k, :)');
+%end
+
+
+grad = grad * (1/m);
+
+%for j =1:400
+ % for k = 1:length(grad)
+  %  grad(k) = 2*(theta(k) - 5);
+  %end
+%end
+% =============================================================
+
+end
